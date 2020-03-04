d69e5665c116ee78fe13e778358cb929dc73a7f6
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-03-04 22:44:05 +0500

    insert,update,delete,search and connectivity to firebase

---
+++ Data.cs
@@ -0,0 +1,8 @@
+﻿namespace firebase
+{
+    internal class Data
+    {
+        public string Id { get; set; }
+        public string Pass { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+namespace firebase
+{
+    public partial class Form1 : Form
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "G4hhYiM1oT9i5JyFcIHZvuEQEmHmu0RqGl3rbjtE",
+            BasePath = "https://fir-connection-ed01b.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            if(client!=null)
+            {
+                MessageBox.Show("successfully established");                
+            }
+
+        }
+  
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id = textBox1.Text,
+                Pass = textBox2.Text
+            };
+            SetResponse res = await client.SetTaskAsync("" + textBox1.Text, data);
+            Data result = res.ResultAs<Data>();
+            MessageBox.Show("inserted....");
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id = textBox1.Text,
+                Pass = textBox2.Text
+            };
+            FirebaseResponse response = await client.GetTaskAsync(textBox1.Text);
+            Data obj = response.ResultAs<Data>();
+            textBox2.Text = obj.Pass;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id = textBox1.Text,
+                Pass = textBox2.Text
+            };
+            //SetResponse res = await client.UpdateTaskAsync("" + textBox1.Text, data);
+            var set = client.Update(textBox1.Text, data);
+            //Data result = res.ResultAs<Data>();
+            MessageBox.Show("updated....");
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            var set = client.Delete(textBox1.Text);
+            MessageBox.Show("deleted....");
+        }
+    }
+}
